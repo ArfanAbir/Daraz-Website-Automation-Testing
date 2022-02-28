e6cec949ad23a9d43774629d9a8c9203eac88f99
Md Nazrul Islam <75695509+ArfanAbir@users.noreply.github.com>
2022-02-28 13:31:47 +0600

    Add files via upload

---
+++ test/specs/sign-up.js
@@ -0,0 +1,65 @@
+describe("sign up daraz", () => {
+
+    it('Daraz', async () => {
+      await browser.url("https://www.daraz.com.bd/")
+    });
+    it('Sign Up', async () => {
+        await $("div[id='anonLogin']").click()
+    });
+
+    it('Register Button',async () => {
+        const link = await $('=Register')
+        link.click()
+    });
+
+
+    it('phone number', async () => {
+      const number =  await $("div[class='mod-input mod-login-input-phone mod-input-phone']")
+      number.setValue("")
+    });
+
+    
+    it('password', async () => {
+        const password =  await $("div[class='mod-input mod-input-password mod-login-input-password mod-input-password']")
+        password.setValue("")
+      });
+
+      
+    it('Month', async () => {
+        const month =  await $("div[id='month']")
+        month.setValue("")
+      });
+
+      it('Day', async () => {
+        const day =  await $("div[id='day']")
+        day.setValue("")
+      });
+
+      it('Year', async () => {
+        const year =  await $("div[id='year']")
+        year.setValue("")
+      });
+
+
+
+      it('Full Name', async () => {
+        const fullName =  await $("div[class='mod-input mod-login-input-name mod-input-name']")
+        fullName.setValue("")
+      });
+
+  
+
+
+      it('Uncheck', async () => {
+        const uncheck =  await $("div[class='mod-login-receive']")
+        uncheck.setValue("")
+      });
+
+
+      
+      it('Submit Button', async () => {
+        const submitButton =  await $("div[class='mod-login-btn']")
+        submitButton.click()
+      });
+    
+});
